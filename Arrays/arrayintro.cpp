ce7c998ae3b2bac711b7d9c3ede3a0f120dc701a
Pankaj Yadav <pankajyadav851685@gmail.com>
2024-12-02 13:40:04 +0530

    arrayintro

---
+++ Arrays/arrayintro.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int num[15];
+    cout<<"first value of num : "<<num[0]<<endl;
+    return 0;
+}
--- basic/ep5_breakandconti.cpp
+++ basic/ep5_breakandconti.cpp
@@ -13,13 +13,30 @@ int main(){
     //     cout<<"prime"<<endl;
     // }
 
-    int num ;
+    // int num ;
+    // cin>>num;
+    // int i;
+    // for(i=2;i<num;i++){
+    //     if(i%2 !=0){
+    //         continue;
+    //     }
+    //     cout<<i<<" ";
+    // }
+
+    int num;
     cin>>num;
-    int i;
-    for(i=2;i<num;i++){
-        if(i%2 !=0){
-            continue;
+
+    int i = 2;
+    
+    for(; i<num;i++){
+        if(num%i==0){
+            cout<<i<<"not prime no "<<endl;
+            break;
         }
-        cout<<i<<" ";
-    }
+         }
+
+        if(num==i){
+            cout<<i<<"prime no"<<endl;
+        }
+   
 }
